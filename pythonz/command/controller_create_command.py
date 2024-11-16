838ec9a2f72fc7f06ec5809465db0a3d7247b113
Michele Capichioni <capimichi@gmail.com>
2024-11-16 22:27:30 +0100

    Refactored `controller_create_command()` to use PythonzManager for generating classes, improving code organization and reducing duplicate code.

--- pythonz/command/controller_create_command.py
+++ pythonz/command/controller_create_command.py
@@ -4,6 +4,7 @@ import click
 
 from pythonz.container.DefaultContainer import DefaultContainer
 from pythonz.helper.ClassHelper import ClassHelper
+from pythonz.manager.PythonzManager import PythonzManager
 
 
 @click.command(
@@ -11,16 +12,8 @@ from pythonz.helper.ClassHelper import ClassHelper
 )
 def controller_create_command():
     default_container: DefaultContainer = DefaultContainer.getInstance()
+    pythonz_manager: PythonzManager = default_container.get(PythonzManager)
 
     controller_name = click.prompt('Enter the name of the controller es. PostController')
-    controller_name = controller_name.replace('Controller', '').replace('controller', '').strip()
-    controller_name = controller_name + 'Controller'
-
-    package_name = default_container.get_config('name')
-    start_dir = os.getcwd()
-
-    path = f"{start_dir}/{package_name}/controller/{controller_name}.py"
-    ClassHelper.create_class_dir(path)
-    ClassHelper.create_class(path, controller_name)
-
+    pythonz_manager.generate_class(controller_name, 'controller')
     click.echo('Created controller successfully')
