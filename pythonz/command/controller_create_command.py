d4bd598b3864a1761d472f3c72b2b82bccaf33bc
Michele Capichioni <capimichi@gmail.com>
2024-11-16 18:56:44 +0100

    Refactored the path for controller package initialization. Moved the init_path to the newly created controller directory.

--- pythonz/command/controller_create_command.py
+++ pythonz/command/controller_create_command.py
@@ -21,7 +21,7 @@ def controller_create_command():
     dir = f"{start_dir}/{package_name}/controller"
     os.makedirs(dir, exist_ok=True)
 
-    init_path = f"{start_dir}/{package_name}/__init__.py"
+    init_path = f"{dir}/__init__.py"
     if(not os.path.exists(init_path)):
         with open(init_path, 'w') as f:
             pass
