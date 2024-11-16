14ad9cf964b9565b7b7f9cb9144600bd6eb8a799
Michele Capichioni <capimichi@gmail.com>
2024-11-16 22:00:22 +0100

    ClassHelper integrated for creating manager classes

--- pythonz/command/manager_create_command.py
+++ pythonz/command/manager_create_command.py
@@ -3,6 +3,7 @@ import os
 import click
 
 from pythonz.container.DefaultContainer import DefaultContainer
+from pythonz.helper.ClassHelper import ClassHelper
 
 
 @click.command(
@@ -18,17 +19,8 @@ def manager_create_command():
     package_name = default_container.get_config('name')
     start_dir = os.getcwd()
 
-    dir = f"{start_dir}/{package_name}/manager"
-    os.makedirs(dir, exist_ok=True)
-
-    init_path = f"{dir}/__init__.py"
-    if (not os.path.exists(init_path)):
-        with open(init_path, 'w') as f:
-            pass
-
-    path = f"{dir}/{manager_name}.py"
-    with open(path, 'w') as f:
-        f.write(f"class {manager_name}:\n")
-        f.write(f"\tpass\n")
+    path = f"{start_dir}/{package_name}/manager/{manager_name}.py"
+    ClassHelper.create_class_dir(path)
+    ClassHelper.create_class(path, manager_name)
 
     click.echo('Created manager successfully')
