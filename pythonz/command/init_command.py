c4b6c570020bfe15b1dd197153766d9decba4623
Michele Capichioni <capimichi@gmail.com>
2024-11-16 17:04:40 +0100

    Refactored init_command to use DefaultContainer and added input prompt for package name.

---
+++ pythonz/command/init_command.py
@@ -0,0 +1,17 @@
+import click
+
+from pythonz.container.DefaultContainer import DefaultContainer
+
+
+@click.command(
+    name='init'
+)
+def init_command():
+
+    default_container: DefaultContainer = DefaultContainer.getInstance()
+    # ask for the name of the package
+    package_name = click.prompt('Enter the name of the package')
+
+    default_container.set_config('name', package_name)
+
+    click.echo('Successfully initialized the package')
