28fc433856adf14bf2de498e895588ce2f716c9b
Michele Capichioni <capimichi@gmail.com>
2024-11-16 17:55:41 +0100

    Refactored the command to create a manager instead of a controller. Updated the commit message accordingly.

---
+++ pythonz/command/manager_create_command.py
@@ -0,0 +1,29 @@
+import os
+
+import click
+
+from pythonz.container.DefaultContainer import DefaultContainer
+
+
+@click.command(
+    name='manager:create'
+)
+def manager_create_command():
+    default_container: DefaultContainer = DefaultContainer.getInstance()
+
+    manager_name = click.prompt('Enter the name of the manager es. PostManager')
+    manager_name = manager_name.replace('Manager', '').replace('manager', '').strip()
+    manager_name = manager_name + 'Manager'
+
+    package_name = default_container.get_config('name')
+    start_dir = os.getcwd()
+
+    dir = f"{start_dir}/{package_name}/manager"
+    os.makedirs(dir, exist_ok=True)
+
+    path = f"{dir}/{manager_name}.py"
+    with open(path, 'w') as f:
+        f.write(f"class {manager_name}:\n")
+        f.write(f"\tpass\n")
+
+    click.echo('Created manager successfully')
