bac42d6cb1a9e94de8e51cdc8d126f6918a8a94e
Michele Capichioni <capimichi@gmail.com>
2024-11-16 17:57:00 +0100

    Added model creation logic to create __init__.py file and write class definition to the model file.

--- pythonz/command/model_create_command.py
+++ pythonz/command/model_create_command.py
@@ -21,6 +21,11 @@ def model_create_command():
     dir = f"{start_dir}/{package_name}/model"
     os.makedirs(dir, exist_ok=True)
 
+    init_path = f"{start_dir}/{package_name}/__init__.py"
+    if (not os.path.exists(init_path)):
+        with open(init_path, 'w') as f:
+            pass
+
     path = f"{dir}/{model_name}.py"
     with open(path, 'w') as f:
         f.write(f"class {model_name}:\n")
