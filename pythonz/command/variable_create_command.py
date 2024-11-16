4e4f5dceeaf163ca2bde1da51212d65ecc1ead97
Michele Capichioni <capimichi@gmail.com>
2024-11-16 22:27:49 +0100

    Refactored VariableCreateCommand to utilize PythonzManager for generating classes, improving code organization and reusability.

--- pythonz/command/variable_create_command.py
+++ pythonz/command/variable_create_command.py
@@ -4,6 +4,7 @@ import click
 
 from pythonz.container.DefaultContainer import DefaultContainer
 from pythonz.helper.ClassHelper import ClassHelper
+from pythonz.manager.PythonzManager import PythonzManager
 
 
 @click.command(
@@ -11,16 +12,8 @@ from pythonz.helper.ClassHelper import ClassHelper
 )
 def variable_create_command():
     default_container: DefaultContainer = DefaultContainer.getInstance()
+    pythonz_manager: PythonzManager = default_container.get(PythonzManager)
 
     variable_name = click.prompt('Enter the name of the variable es. PostVariable')
-    variable_name = variable_name.replace('Variable', '').replace('variable', '').strip()
-    variable_name = variable_name + 'Variable'
-
-    package_name = default_container.get_config('name')
-    start_dir = os.getcwd()
-
-    path = f"{start_dir}/{package_name}/variable/{variable_name}.py"
-    ClassHelper.create_class_dir(path)
-    ClassHelper.create_class(path, variable_name, 'str')
-
+    pythonz_manager.generate_class(variable_name, 'variable')
     click.echo('Created variable successfully')
