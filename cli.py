5e00c82f5bc7981a2fdf53914cc50ec0eefc9a19
Michele Capichioni <capimichi@gmail.com>
2024-11-16 17:55:18 +0100

    ControllerCreateCommand added to CLI.

--- cli.py
+++ cli.py
@@ -1,14 +1,14 @@
 import click
 
 from pythonz.command.init_command import init_command
+from pythonz.command.controller_create_command import controller_create_command
 
 @click.group()
 def cli():
     pass
 
 cli.add_command(init_command)
-# cli.add_command(command2)
-# cli.add_command(command3)
+cli.add_command(controller_create_command)
 
 if __name__ == '__main__':
     cli()
