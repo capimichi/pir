9c40d1d29e46f9c6d1956a5f6131c5118bf35652
Michele Capichioni <capimichi@gmail.com>
2024-11-16 17:55:54 +0100

    Renamed command from 'controller:create' to 'variable:create' and updated the file accordingly.

---
+++ pythonz/command/variable_create_command.py
@@ -0,0 +1,29 @@
+import os
+
+import click
+
+from pythonz.container.DefaultContainer import DefaultContainer
+
+
+@click.command(
+    name='variable:create'
+)
+def variable_create_command():
+    default_container: DefaultContainer = DefaultContainer.getInstance()
+
+    variable_name = click.prompt('Enter the name of the variable es. PostVariable')
+    variable_name = variable_name.replace('Variable', '').replace('variable', '').strip()
+    variable_name = variable_name + 'Variable'
+
+    package_name = default_container.get_config('name')
+    start_dir = os.getcwd()
+
+    dir = f"{start_dir}/{package_name}/variable"
+    os.makedirs(dir, exist_ok=True)
+
+    path = f"{dir}/{variable_name}.py"
+    with open(path, 'w') as f:
+        f.write(f"class {variable_name}(str):\n")
+        f.write(f"\tpass\n")
+
+    click.echo('Created variable successfully')
