4759775bdfa80bb87908c2d08af9076571369832
Michele Capichioni <capimichi@gmail.com>
2024-11-16 23:13:26 +0100

    updated setup.py for pythonz package, including dependencies and entry points

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pythonz",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "click",
+    ],
+    entry_points={
+        "console_scripts": [
+            "pythonz=cli:cli",
+        ],
+    },
+    url="https://github.com/capimichi/pythonz",
+    author="Michele Capicchioni",
+    author_email="capimichi@gmail.com",
+    description="Pythonz is a simple CLI tool to generate Python classes",
+)
