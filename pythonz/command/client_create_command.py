ab0863ff7ec5f0d37f3bf2930d0bcabbeac51a4b
Michele Capichioni <capimichi@gmail.com>
2024-11-17 12:31:50 +0100

    Added optional 'entity_name' parameter to 'client:create' command and updated the class generation logic accordingly.

--- pythonz/command/client_create_command.py
+++ pythonz/command/client_create_command.py
@@ -8,12 +8,18 @@ from pythonz.manager.PythonzManager import PythonzManager
 
 
 @click.command(
-    name='client:create'
+    name='client:create',
 )
-def client_create_command():
+@click.option('--entity_name', prompt='Enter the name of the entity (optional)', default='',
+              help='The name of the entity associated with the client')
+def client_create_command(entity_name: str):
     default_container: DefaultContainer = DefaultContainer.getInstance()
     pythonz_manager: PythonzManager = default_container.get(PythonzManager)
 
     client_name = click.prompt('Enter the name of the client es. PostClient')
-    pythonz_manager.generate_class(client_name, 'client')
+    pythonz_manager.generate_class(
+        class_name=client_name,
+        class_type='client',
+        entity_name=entity_name
+    )
     click.echo('Created client successfully')
