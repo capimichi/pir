de93bfde2739d2e3c1a9e9d9a52344a24ad8b2d1
Michele Capichioni <capimichi@gmail.com>
2024-11-17 12:35:51 +0100

    Added --entity_name option to controller:create command for specifying the associated entity name

--- pythonz/command/controller_create_command.py
+++ pythonz/command/controller_create_command.py
@@ -10,10 +10,15 @@ from pythonz.manager.PythonzManager import PythonzManager
 @click.command(
     name='controller:create'
 )
-def controller_create_command():
+@click.option('--entity_name', default='', help='The name of the entity associated with the controller')
+def controller_create_command(entity_name: str):
     default_container: DefaultContainer = DefaultContainer.getInstance()
     pythonz_manager: PythonzManager = default_container.get(PythonzManager)
 
     controller_name = click.prompt('Enter the name of the controller es. PostController')
-    pythonz_manager.generate_class(controller_name, 'controller')
+    pythonz_manager.generate_class(
+        class_name=controller_name,
+        class_type='controller',
+        entity_name=entity_name
+    )
     click.echo('Created controller successfully')
