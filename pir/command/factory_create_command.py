0f25276e062f2775302487b780c23a0ff19a43ce
Michele Capichioni <capimichi@gmail.com>
2024-11-23 14:50:57 +0100

    Refactored code to improve performance

--- pir/command/factory_create_command.py
+++ pir/command/factory_create_command.py
@@ -10,10 +10,14 @@ from pir.manager.PirManager import PirManager
 @click.command(
     name='factory:create'
 )
-def factory_create_command():
+@click.option('--name', default='', help='The name of the factory',
+              prompt="Enter the name of the factory. es. PostFactory")
+def factory_create_command(name: str):
     default_container: DefaultContainer = DefaultContainer.getInstance()
     pir_manager: PirManager = default_container.get(PirManager)
 
-    factory_name = click.prompt('Enter the name of the factory es. PostFactory')
-    pir_manager.generate_class(factory_name, 'factory')
+    pir_manager.generate_class(
+        name,
+        'factory'
+    )
     click.echo('Created factory successfully')
