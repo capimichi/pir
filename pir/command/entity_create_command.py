6c645fa64d7fc5bd9c21b9afcae9c4c81ab4cc3a
Michele Capichioni <capimichi@gmail.com>
2024-11-23 14:50:56 +0100

    Fixed bug in function X, improved performance by optimizing algorithm Y.

--- pir/command/entity_create_command.py
+++ pir/command/entity_create_command.py
@@ -10,10 +10,14 @@ from pir.manager.PirManager import PirManager
 @click.command(
     name='entity:create'
 )
-def entity_create_command():
+@click.option('--name', default='', help='The name of the entity',
+              prompt="Enter the name of the entity. es. PostEntity")
+def entity_create_command(name: str):
     default_container: DefaultContainer = DefaultContainer.getInstance()
     pir_manager: PirManager = default_container.get(PirManager)
 
-    entity_name = click.prompt('Enter the name of the entity es. PostEntity')
-    pir_manager.generate_class(entity_name, 'entity')
+    pir_manager.generate_class(
+        name,
+        'entity'
+    )
     click.echo('Created entity successfully')
