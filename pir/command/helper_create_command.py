3aee93f6613223a5efc7b97e65059fff4847146c
Michele Capichioni <capimichi@gmail.com>
2024-11-23 14:50:58 +0100

    Updated UI components to improve user experience

--- pir/command/helper_create_command.py
+++ pir/command/helper_create_command.py
@@ -10,10 +10,14 @@ from pir.manager.PirManager import PirManager
 @click.command(
     name='helper:create'
 )
-def helper_create_command():
+@click.option('--name', default='', help='The name of the helper',
+              prompt="Enter the name of the helper. es. PostHelper")
+def helper_create_command(name: str):
     default_container: DefaultContainer = DefaultContainer.getInstance()
     pir_manager: PirManager = default_container.get(PirManager)
 
-    helper_name = click.prompt('Enter the name of the helper es. PostHelper')
-    pir_manager.generate_class(helper_name, 'helper')
+    pir_manager.generate_class(
+        name,
+        'helper'
+    )
     click.echo('Created helper successfully')
