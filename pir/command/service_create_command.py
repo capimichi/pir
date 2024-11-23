05fc71d093db8246f8e69c680b1cabb757686fc6
Michele Capichioni <capimichi@gmail.com>
2024-11-23 14:51:02 +0100

    Fixed bug in function XYZ by updating variable assignment.

--- pir/command/service_create_command.py
+++ pir/command/service_create_command.py
@@ -10,14 +10,14 @@ from pir.manager.PirManager import PirManager
 @click.command(
     name='service:create'
 )
+@click.option('--name', default='', help='The name of the service', prompt="Enter the name of the service. es. PostService")
 @click.option('--entity_name', default='', help='The name of the entity associated with the service')
-def service_create_command(entity_name: str):
+def service_create_command(name: str, entity_name: str):
     default_container: DefaultContainer = DefaultContainer.getInstance()
     pir_manager: PirManager = default_container.get(PirManager)
 
-    service_name = click.prompt('Enter the name of the service es. PostService')
     pir_manager.generate_class(
-        class_name=service_name,
+        class_name=name,
         class_type='service',
         entity_name=entity_name
     )
