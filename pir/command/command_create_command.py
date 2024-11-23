76ea59a7b42211fe42316ada0ffa16660b630ddc
Michele Capichioni <capimichi@gmail.com>
2024-11-23 14:50:54 +0100

    Update UI to improve readability

--- pir/command/command_create_command.py
+++ pir/command/command_create_command.py
@@ -10,11 +10,11 @@ from pir.manager.PirManager import PirManager
 @click.command(
     name='command:create'
 )
-def command_create_command():
+@click.option('--name', default='', help='The name of the command', prompt="Enter the name of the command. es. create_post_command")
+@click.option('--command_alias', default='', help='The alias of the command', prompt="Enter the alias of the command. es. create:post")
+def command_create_command(name: str, command_alias: str):
     default_container: DefaultContainer = DefaultContainer.getInstance()
     pir_manager: PirManager = default_container.get(PirManager)
 
-    command_name = click.prompt('Enter the name of the command es. create_post_command')
-    command_alias = click.prompt('Enter the alias of the command es. post:create')
-    pir_manager.generate_command(command_name, command_alias)
+    pir_manager.generate_command(name, command_alias)
     click.echo('Created command successfully')
