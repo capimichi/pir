a136105be81e02fc4cdb4bc681e0b6da64aac512
Michele Capichioni <capimichi@gmail.com>
2024-11-23 14:51:00 +0100

    Updated UI design to improve user experience

--- pir/command/repository_create_command.py
+++ pir/command/repository_create_command.py
@@ -10,10 +10,13 @@ from pir.manager.PirManager import PirManager
 @click.command(
     name='repository:create'
 )
-def repository_create_command():
+@click.option('--name', default='', help='The name of the repository', prompt="Enter the name of the repository. es. PostRepository")
+def repository_create_command(name: str):
     default_container: DefaultContainer = DefaultContainer.getInstance()
     pir_manager: PirManager = default_container.get(PirManager)
 
-    repository_name = click.prompt('Enter the name of the repository es. PostRepository')
-    pir_manager.generate_class(repository_name, 'repository')
+    pir_manager.generate_class(
+        name,
+        'repository'
+    )
     click.echo('Created repository successfully')
