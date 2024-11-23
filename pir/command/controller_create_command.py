3c5ae4dee040b49947f2bbdf8f5d2962ec7c674c
Michele Capichioni <capimichi@gmail.com>
2024-11-23 14:50:55 +0100

    Updated UI design and added new features for improved user experience

--- pir/command/controller_create_command.py
+++ pir/command/controller_create_command.py
@@ -10,14 +10,14 @@ from pir.manager.PirManager import PirManager
 @click.command(
     name='controller:create'
 )
+@click.option('--name', default='', help='The name of the controller', prompt="Enter the name of the controller. es. PostController")
 @click.option('--entity_name', default='', help='The name of the entity associated with the controller')
-def controller_create_command(entity_name: str):
+def controller_create_command(name: str, entity_name: str):
     default_container: DefaultContainer = DefaultContainer.getInstance()
     pir_manager: PirManager = default_container.get(PirManager)
 
-    controller_name = click.prompt('Enter the name of the controller es. PostController')
     pir_manager.generate_class(
-        class_name=controller_name,
+        class_name=name,
         class_type='controller',
         entity_name=entity_name
     )
