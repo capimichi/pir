bfb34e91e84ef4daa583be05afc50b86e71408b4
Michele Capicchioni <capimichi@gmail.com>
2024-12-09 19:12:09 +0100

    Update app_init_command.py

--- pir/command/app_init_command.py
+++ pir/command/app_init_command.py
@@ -9,7 +9,7 @@ from pir.manager.PirManager import PirManager
 @click.command(
     name='app:init'
 )
-def cli_init_command():
+def app_init_command():
     default_container: DefaultContainer = DefaultContainer.getInstance()
     pir_manager: PirManager = default_container.get(PirManager)
     pir_manager.init_app()
